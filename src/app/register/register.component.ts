081892e42f2e409e33eefb93e563af83aaf6de6b
Silene Nazareth Casma Rubio <u20201a213@upc.edu.pe>
2023-07-25 13:04:08 -0500

    Add: udpating same changes in register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  togglePasswordVisibility() {
+    const lockIcon = document.getElementById("lock") as HTMLElement;
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
 
+    if (passwordInput.type !== "password") {
+      passwordInput.type = "password";
+      lockIcon.classList.remove("bxs-lock-open-alt");
+      lockIcon.classList.add("bxs-lock-alt");
+    } else {
+      passwordInput.type = "text";
+      lockIcon.classList.remove("bxs-lock-alt");
+      lockIcon.classList.add("bxs-lock-open-alt");
+    }
+  }
 }
