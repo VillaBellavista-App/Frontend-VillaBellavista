58b842e440492399542ae55c37fd0cce66f4318b
Silene Nazareth Casma Rubio <u20201a213@upc.edu.pe>
2023-07-25 11:39:53 -0500

    Add: toggle password visibility

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  togglePasswordVisibility() {
+    const lockIcon = document.getElementById("lock") as HTMLElement;
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+
+    if (passwordInput.type !== "password") {
+      passwordInput.type = "password";
+      lockIcon.classList.remove("bxs-lock-open-alt");
+      lockIcon.classList.add("bxs-lock-alt");
+    } else {
+      passwordInput.type = "text";
+      lockIcon.classList.remove("bxs-lock-alt");
+      lockIcon.classList.add("bxs-lock-open-alt");
+    }
+  }
 
 }
