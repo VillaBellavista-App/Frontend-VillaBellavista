c884d47c7619702de5f408917433cb229aaf3869
silene <u20201a213@upc.edu.pe>
2023-09-19 13:55:30 -0500

    Add: final validations

--- src/app/departure-dialog/departure-dialog.component.ts
+++ src/app/departure-dialog/departure-dialog.component.ts
@@ -6,6 +6,7 @@ import { VehiculesService } from '../services/vehicules.service';
 import { TicketService } from '../services/ticket.service';
 import { Vehicule } from '../models/vehicule';
 import { Location } from '@angular/common';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-departure-dialog',
@@ -14,6 +15,7 @@ import { Location } from '@angular/common';
 })
 
 export class DepartureDialogComponent implements OnInit {
+  departureForm!: FormGroup;
   editedDeparture: Ticket;
   isEditMode: boolean;
   vehicle_response: TicketPost | undefined;
@@ -21,7 +23,8 @@ export class DepartureDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<DepartureDialogComponent>, private vehicleService: VehiculesService, private ticketService: TicketService,
-    @Inject(MAT_DIALOG_DATA) public data: Ticket, private location: Location
+    @Inject(MAT_DIALOG_DATA) public data: Ticket, private location: Location,
+    private formBuilder: FormBuilder
   ) {
     this.isEditMode = !!data; // Si data tiene valor, estamos en modo edición
     this.editedDeparture = this.isEditMode ? { ...data } : {} as Ticket;
@@ -29,20 +32,31 @@ export class DepartureDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.getVehiculesData();
+    this.createForm();
   }
 
+  createForm() {
+    this.departureForm = this.formBuilder.group({
+      tic_placa: [
+        this.editedDeparture.tic_placa,
+        [Validators.required,]
+      ],
+    });
+  }
   onSave(): void {
-    const plate = this.editedDeparture.tic_placa;
+    if (this.departureForm.valid) {
+      this.editedDeparture = this.departureForm.value;
 
-    this.vehicleService.getVehicleIdByPlate(plate).subscribe(
-      (vehicleIds: Vehicule[]) => {
+      const plate = this.editedDeparture.tic_placa;
+      this.vehicleService.getVehicleIdByPlate(plate).subscribe(
+        (vehicleIds: Vehicule[]) => {
 
           const vehicle_id = vehicleIds[0].veh_id; // Obtener el veh_id del primer elemento del array
           const newTicket: TicketPost = {
             vehicule_id: vehicle_id,
             // Otras propiedades del TicketPost que desees inicializar...
           };
-          
+
           // Asignar el objeto newTicket a vehicle_response
           this.vehicle_response = newTicket;
 
@@ -58,12 +72,15 @@ export class DepartureDialogComponent implements OnInit {
           // Cerrar el diálogo después de realizar la operación
           this.dialogRef.close(this.editedDeparture);
           window.location.reload();
-      },
-      error => {
-        console.error('Error al obtener el ID del vehículo:', error);
-        // Manejar el error si es necesario
-      }
-    );
+        },
+        error => {
+          console.error('Error al obtener el ID del vehículo:', error);
+          // Manejar el error si es necesario
+        }
+      );
+    }else {
+      console.error('El formulario no es válido. Por favor, complete todos los campos.');
+    }
   }
 
   onCancel(): void {
